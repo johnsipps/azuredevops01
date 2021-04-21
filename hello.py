67102c775fa65a2569de2cc8833d36496d8059d3
johnsipps <johnsipps@gmail.com>
2021-04-21 09:25:29 +0530

    changes

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ import datetime
 os.system('clear')
 now = datetime.datetime.now()
 timestamp = str(now.strftime("%Y%m%d_%H-%M-%S"))
-flog = open("status.log."+timestamp,"a")
-ferror = open("error.log"+timestamp,"a")
+flog = open("run_logs/status.log."+timestamp,"a")
+ferror = open("error_logs/error.log"+timestamp,"a")
 a=3
 if a == 1:
     flog.write('hello john and ciciliya')
